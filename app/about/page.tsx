6350600a0c344d9e9ef31dc086a4b0711faab91f
Deonte Palmer <ferfrink3577@gmail.com>
2024-07-17 01:35:41 -0500

    :sparkles: init

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-4xl font-bold text-center">About</h1>
+      </main>
+    </>
+  );
+}
